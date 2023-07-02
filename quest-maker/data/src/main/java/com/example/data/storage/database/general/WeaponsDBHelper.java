b5cb92b0e67f9144c29b79682dcc04314062c5ce
KochubeyRoman <kochubey.roma@gmail.com>
2023-07-02 13:41:32 +0300

    added one weapon (weapDB) for future testing

--- quest-maker/data/src/main/java/com/example/data/storage/database/general/WeaponsDBHelper.java
+++ quest-maker/data/src/main/java/com/example/data/storage/database/general/WeaponsDBHelper.java
@@ -8,11 +8,13 @@ import android.database.sqlite.SQLiteOpenHelper;
 // (?) - don't know
 // (#) - testing
 
-public class weaponsDBHelper extends SQLiteOpenHelper {
+public class WeaponsDBHelper extends SQLiteOpenHelper {
 
     public static final String DB_NAME = "weaponsDB";
     public static final String TABLE_NAME = "weaponsTable";
     public static final int DB_VERSION = 1;
+
+    // TODO("Добавить поле дял добавления картинки")
 
     public static final String KEY_ID = "_id";
     public static final String KEY_NAME = "name";
@@ -22,10 +24,11 @@ public class weaponsDBHelper extends SQLiteOpenHelper {
     public static final String KEY_DAMAGE = "damage";
     public static final String KEY_SPEED = "speed";
     public static final String KEY_HITCHANCE = "hit-chance";
+    public static final String KEY_THROWDAMAGE = "throw-damage";
     public static final String KEY_SELLPRICE = "sell-price";
     public static final String KEY_DESCR = "description";
 
-    public weaponsDBHelper(Context context) {
+    public WeaponsDBHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
     }
 
@@ -41,9 +44,35 @@ public class weaponsDBHelper extends SQLiteOpenHelper {
                 + KEY_DAMAGE + " integer,"
                 + KEY_SPEED + " integer,"
                 + KEY_HITCHANCE + " integer,"
+                + KEY_THROWDAMAGE + " integer,"
                 + KEY_SELLPRICE + " integer,"
                 + KEY_DESCR  + " text,"
                 + ") ");
+
+        sqLiteDatabase.execSQL("insert into " + TABLE_NAME
+                + "("
+                + KEY_NAME        + ", "
+                + KEY_DAMAGETYPE  + ", "
+                + KEY_HANDED      + ", "
+                + KEY_PREFSTAT    + ", " // (*) - Влияет на скорость использования оружия
+                + KEY_DAMAGE      + ", "
+                + KEY_SPEED       + ", "
+                + KEY_HITCHANCE   + ", "
+                + KEY_SELLPRICE   + ", "
+                + KEY_DESCR       + ") "
+                + "VALUES "
+
+                + "('Baseball Bat', 'BLUNT', 2, 'STR', 4, 95, 80, 4, 1, 'Baseball bat used by schools team.')"
+                /*+ "('STA', 21), "   // Stamina
+                + "('REA', 21), "   // Reason
+                + "('FND', 6), "    // Money
+                //+ "('LVL', 1), "  // Level
+                //+ "('EXP', 0), "  // Experience
+                //+ "('DOOM' 0), "  // Doom
+                + "('BUL', 0), "    // Bullets
+                + "('CIG', 0)"      // Cigarettes*/
+        );
+
     }
 
     @Override
