96d39af2db8b326f107be89bcdc72eaf5807deca
KochubeyRoman <kochubey.roma@gmail.com>
2023-07-16 00:15:33 +0300

    one more weapon

--- quest-maker/data/src/main/java/com/example/data/storage/database/database/general/WeaponDBHelper.java
+++ quest-maker/data/src/main/java/com/example/data/storage/database/database/general/WeaponDBHelper.java
@@ -71,7 +71,8 @@ public class WeaponDBHelper extends SQLiteOpenHelper {
                         + "values "
 
                         + "('Baseball Bat', 'BLUNT', 2, 'STR', 4, 95, 20, 80, 4, 1, 'Baseball bat used by schools team.'),"
-                        + "('ABOBA', 'BLUNT', 2, 'STR', 4, 95, 20, 80, 4, 1, 'Baseball bat used by schools team.')");
+                        + "('ABOBA', 'BLUNT', 2, 'STR', 4, 95, 20, 80, 4, 1, 'Baseball bat used by schools team.'),"
+                        + "('BIBA', 'BLUNT', 2, 'STR', 4, 95, 20, 80, 4, 1, 'Baseball bat used by schools team.')");
 
     }
 
