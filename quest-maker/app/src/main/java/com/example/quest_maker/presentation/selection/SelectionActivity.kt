30f3fa46fa4faf0b3de728a60d125212b3685a9f
KochubeyRoman <kochubey.roma@gmail.com>
2023-07-15 15:10:00 +0300

    add rv_item_radio button and bottom nav

--- quest-maker/app/src/main/java/com/example/quest_maker/presentation/selection/SelectionActivity.kt
+++ quest-maker/app/src/main/java/com/example/quest_maker/presentation/selection/SelectionActivity.kt
@@ -1,8 +1,11 @@
 package com.example.quest_maker.presentation.selection
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.quest_maker.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class SelectionActivity : AppCompatActivity() {
 
@@ -11,11 +14,30 @@ class SelectionActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_selection)
 
-
+        val bottomNavigation : BottomNavigationView = findViewById(R.id.selection_navigation_bar)
+        bottomNavigation.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
 
 
 
     }
 
+    fun onNavigationItemSelected(item: MenuItem): Boolean {
+        displayView(item.itemId)
+        return true
+    }
+
+    fun displayView(viewId: Int) {
+        var fragment: Fragment? = null
+        when (viewId) {
+            R.id.inv -> fragment = DictionaryFragment()
+            R.id.collection -> fragment = CollectionFragment()
+            R.id.game -> fragment = GameFragment()
+        }
+        if (fragment != null) {
+            val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
+            ft.replace(R.id.fragment_container_view, fragment)
+            ft.commit()
+        }
+    }
 
 }
